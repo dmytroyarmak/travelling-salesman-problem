432a6c0ebb4fc42ce6bf02fb9f9d0088edc6b314
Dmytro Yarmak <dmytroyarmak@gmail.com>
2014-11-09 21:51:10 +0200

    Add initial solutions

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 var loadSample = require('./lib/tsplib').loadSample;
 
-loadSample('br17').then(function (sampleData) {
+var SAMPLES = [
+  'br17',
+  'bays29',
+  'ftv33',
+  'ftv35',
+  'swiss42',
+  'p43',
+  'ftv44',
+  'ftv47',
+  'att48',
+  'ry48p',
+  'eil51',
+  'berlin52',
+  'ft53',
+  'ftv55',
+  'ftv64',
+  'eil76',
+  'eil101'
+];
+
+loadSample(SAMPLES[0]).then(function (sampleData) {
   console.log(sampleData);
 });
